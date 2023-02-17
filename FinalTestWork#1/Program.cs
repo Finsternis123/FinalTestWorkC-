bedb7cf5d796daf6ac6215fe2883a1ab4e07aeb0
Vladislav <zomba4o4ek100@gmail.com>
2023-02-17 15:49:26 +0300

    First method

--- FinalTestWork#1/Program.cs
+++ FinalTestWork#1/Program.cs
@@ -1,2 +1,15 @@
 ﻿string[] Array = new string[4] {"hello", "2", "world", ":-)"};
 string[] Array1 = new string[Array.Length];
+
+void SortArray (string[] Array, string[] Array1)
+{
+    int count = 0;
+    for (int i = 0; i < Array.Length; i++)
+    {
+        if (Array[i].Length <= 3)
+        {
+            Array1[count] = Array[i];
+            count++;
+        }
+    }
+}
