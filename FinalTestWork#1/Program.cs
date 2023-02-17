0ace62655c04047e8dc08efa8134c1c095f8b41b
Vladislav <zomba4o4ek100@gmail.com>
2023-02-17 16:08:26 +0300

    Second  method

--- FinalTestWork#1/Program.cs
+++ FinalTestWork#1/Program.cs
@@ -13,3 +13,15 @@ void SortArray (string[] Array, string[] Array1)
         }
     }
 }
+
+void PrintArray (string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+SortArray(Array, Array1);
+PrintArray(Array1);
